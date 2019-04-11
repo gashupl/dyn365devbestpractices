a5e599aa3b737394cd309a854a83e5b0fad54919
Piotr Gaszewski <piog@netcompany.com>
2019-04-11 08:52:30 +0200

    PluginExecutionContextExtensions class added

---
+++ XrmLabs.Blog.Dyn365BestPractices/Chapter 01/Chapter01.FooPlugin/Extensions/IPluginExecutionContextExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace Chapter01.FooPlugin.Extensions
+{
+    public static class IPluginExecutionContextExtensions
+    {
+        public static string ToFormattedString(this IPluginExecutionContext pluginExecutionContext)
+        {
+            throw new NotImplementedException(); 
+        } 
+    }
+}
--- XrmLabs.Blog.Dyn365BestPractices/Chapter 01/Chapter01.FooPlugin/PluginBase.cs
+++ XrmLabs.Blog.Dyn365BestPractices/Chapter 01/Chapter01.FooPlugin/PluginBase.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
+using Chapter01.FooPlugin.Extensions;
 using Microsoft.Xrm.Sdk;
 
 namespace Chapter01.FooPlugin
@@ -51,53 +52,63 @@ namespace Chapter01.FooPlugin
                 throw new ArgumentNullException(nameof(serviceProvider));
             }
 
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            if (tracingService == null)
+            {
+                throw new ApplicationException("Failed to initialize plugin tracing service");
+            }
+
+            IPluginExecutionContext pluginExecutionContext = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            if (pluginExecutionContext == null)
+            {
+                throw new ApplicationException("Failed to initialize plugin execution context");
+            }
+
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService organizationService = serviceFactory.CreateOrganizationService(pluginExecutionContext.UserId);
+
+            // Resolve the Azure service bus notification service 
+            var notificationService = (IServiceEndpointNotificationService)serviceProvider.GetService(typeof(IServiceEndpointNotificationService));
+            if (notificationService == null)
+            {
+                throw new ApplicationException("Failed to initialize Azure service bus notification service");
+            }
+
             try
             {
-
-                ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
-                if (tracingService == null)
-                {
-                    throw new ApplicationException("Failed to initialize plugin tracing service");  
-                }
-
-                IPluginExecutionContext pluginExecutionContext = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
-                if (pluginExecutionContext == null)
-                {
-                    throw new ApplicationException("Failed to initialize plugin execution context");
-                }
-
-                IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-                IOrganizationService organizationService = serviceFactory.CreateOrganizationService(pluginExecutionContext.UserId);
-
-                // Resolve the Azure service bus notification service 
-                var notificationService = (IServiceEndpointNotificationService)serviceProvider.GetService(typeof(IServiceEndpointNotificationService));
-                if (notificationService == null)
-                {
-                    throw new ApplicationException("Failed to initialize Azure service bus notification service");
-                }
-
                 if (this.IsContextValid(pluginExecutionContext))
                 {
-                    this.Execute(pluginExecutionContext, tracingService); 
+                    this.Execute(pluginExecutionContext, tracingService);
                 }
                 else
                 {
                     // Perform silent abort
                     tracingService.Trace($"Invalid plugin execution context detected (Plugin: {this.Name})");
-                    // tracingService.Trace($"Execution context: [{pluginExecutionContext.ToFormattedString()}]");
+                    tracingService.Trace($"Execution context: [{pluginExecutionContext.ToFormattedString()}]");
                     tracingService.Trace("Plugin execution aborted");
                     return;
                 }
             }
-            catch (InvalidPluginExecutionException) { throw; }
-            catch (OutOfMemoryException e) { throw new InvalidPluginExecutionException(OperationStatus.Failed, e.Message); }
-            catch (StackOverflowException e) { throw new InvalidPluginExecutionException(OperationStatus.Failed, e.Message); }
-            catch (ThreadAbortException e) { throw new InvalidPluginExecutionException(OperationStatus.Failed, e.Message); }
+            catch (InvalidPluginExecutionException)
+            {
+                throw;
+            }
+            catch (OutOfMemoryException e)
+            {
+                throw new InvalidPluginExecutionException(OperationStatus.Failed, e.Message);
+            }
+            catch (StackOverflowException e)
+            {
+                throw new InvalidPluginExecutionException(OperationStatus.Failed, e.Message);
+            }
+            catch (ThreadAbortException e)
+            {
+                throw new InvalidPluginExecutionException(OperationStatus.Failed, e.Message);
+            }
             catch (Exception e)
             {
-                //tracingService?.Trace($"Plugin failed unexpectedly: '{this.Name}'");
-                //tracingService.Trace($"Exception: {e}");
-
+                tracingService?.Trace($"Plugin failed unexpectedly: '{this.Name}'");
+                tracingService.Trace($"Exception: {e}");
                 throw new InvalidPluginExecutionException(OperationStatus.Failed, "Sorry, the action failed unexpectedly!");
             }
         }

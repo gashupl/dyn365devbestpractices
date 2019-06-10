f43b9cc7f9c2b7b4120545058bedc6915bf4198e
Piotr Gaszewski <piog@netcompany.com>
2019-06-10 14:54:08 +0200

    Foo Plugin add Service added

---
+++ XrmLabs.Blog.Dyn365BestPractices/Chapter03/Chapter03.Plugins/FooPlugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chapter01.FooPlugin;
+using Chapter02.Repositories;
+using Microsoft.Xrm.Sdk;
+
+namespace Chapter03.Plugins
+{
+    public class FooPlugin : PluginBase
+    {
+        #region Constructor
+        public FooPlugin(string unsecureString, string secureString) : base(unsecureString, secureString)
+        {
+        } 
+        #endregion
+
+        public override bool IsContextValid(IPluginExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute(IPluginExecutionContext pluginExecutionContext, ITracingService tracingService)
+        {
+            //var opportunityRepo = new OpportunityRepository(this.)
+            //{
+
+            //var fooService = new FooLogicService()
+
+        }
+    }
+}

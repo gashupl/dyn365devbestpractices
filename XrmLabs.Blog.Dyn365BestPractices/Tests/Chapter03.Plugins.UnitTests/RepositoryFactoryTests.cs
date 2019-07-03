136305d3cc53646a9fd18dc5be122e4a7d704d5e
Piotr Gaszewski <piotrek.gaszewski@hotmail.com>
2019-07-03 19:52:49 +0200

    Unit test added for RepositoryFactory Get method

---
+++ XrmLabs.Blog.Dyn365BestPractices/Tests/Chapter03.Plugins.UnitTests/RepositoryFactoryTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakeXrmEasy;
+using Chapter03.Plugins.Sample3;
+using Common.Entities;
+using Chapter02.Repositories;
+
+namespace Chapter03.Plugins.UnitTests
+{
+    [TestClass]
+    public class RepositoryFactoryTests
+    {
+        [TestMethod]
+        public void Execute_CreateOpportunityRepository_ReturnValidObject()
+        {
+            var context = new XrmFakedContext();
+            var orgService = context.GetOrganizationService();
+
+            var factory = new RepositoryFactory(orgService);
+            var opportunityRepo = factory.Get<Opportunity, OpportunityRepository>();
+
+            Assert.IsInstanceOfType(opportunityRepo, typeof(OpportunityRepository)); 
+
+        }
+    }
+}

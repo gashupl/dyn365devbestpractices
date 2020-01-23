f8802db6ee4775a9fa4cb617e9e10e682fdec17d
Piotr Gaszewski <piog@netcompany.com>
2020-01-23 07:10:19 +0100

    Signing assembly with private keys

--- XrmLabs.Blog.Dyn365BestPractices/Chapter06/Chapter06.Plugins/Repositories/TeamRepository.cs
+++ XrmLabs.Blog.Dyn365BestPractices/Chapter06/Chapter06.Plugins/Repositories/TeamRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Common.Entities;
 using Chapter06.Plugins.Repositories.Base; 
 

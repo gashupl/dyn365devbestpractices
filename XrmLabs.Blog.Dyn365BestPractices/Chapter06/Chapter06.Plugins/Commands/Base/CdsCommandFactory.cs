6bf337be2cf0feabf7a1c2dd10fbbc4101d33efd
gashupl <piotrek.gaszewski@hotmail.com>
2020-02-07 08:24:04 +0100

    Protected variable name changed

--- XrmLabs.Blog.Dyn365BestPractices/Chapter06/Chapter06.Plugins/Commands/Base/CdsCommandFactory.cs
+++ XrmLabs.Blog.Dyn365BestPractices/Chapter06/Chapter06.Plugins/Commands/Base/CdsCommandFactory.cs
@@ -8,18 +8,18 @@ namespace Chapter06.Plugins.Commands.Base
     {
         protected ICdsServiceProvider serviceProvider { get; private set; }
 
-        protected ICdsRepositoryFactory CdsRepositoryFactory { get; private set; }
+        protected ICdsRepositoryFactory cdsRepositoryFactory { get; private set; }
 
         public CdsCommandFactory(ICdsServiceProvider serviceProvider, ICdsRepositoryFactory cdsRepositoryFactory)
         {
             this.serviceProvider = serviceProvider;
-            this.CdsRepositoryFactory = cdsRepositoryFactory ?? new CdsRepositoryFactory(this.serviceProvider);
+            this.cdsRepositoryFactory = cdsRepositoryFactory ?? new CdsRepositoryFactory(this.serviceProvider);
         }
 
         public ICdsCommand GetCommand<T>()  where T : CdsCommandBase, new()
         {
             T command = new T();
-            command.Initialize(this.serviceProvider.Context, this.serviceProvider.TracingService, this.CdsRepositoryFactory);
+            command.Initialize(this.serviceProvider.Context, this.serviceProvider.TracingService, this.cdsRepositoryFactory);
             return command;
         }
     }
